8c03306e05cd77c5c0ae2ff40a1596064246709d
Adarsh Singh <mrideal000@gmail.com>
2025-02-07 13:53:19 +0530

    undo action

--- src/Components/Navabar/Navbar.jsx
+++ src/Components/Navabar/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
     <>
       <div className="navbar">
         <div className="title nav-title">
-          GlobjjjjlBites <FontAwesomeIcon icon={faUtensils} />
+          GloblBites <FontAwesomeIcon icon={faUtensils} />
         </div>
         <div className="nav-link">
           <ul>
